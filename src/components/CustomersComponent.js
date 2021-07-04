ea6e20d49dceffb03fbdc381244903c66c25d17e
Rodrick Coleman <rscole87@gmail.com>
2021-07-04 19:15:29 -0400

    add new customer button

--- src/components/CustomersComponent.js
+++ src/components/CustomersComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { ListGroup } from "reactstrap";
+import { ListGroup, Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Input, Label } from "reactstrap";
 import { RenderCustomerLi, RenderCustomerProfile } from "./Customer";
 import CUSTOMERLIST from "../shared/customerList";
 
@@ -7,11 +7,17 @@ class CustomerSection extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      isModalOpen: false,
       customerList: CUSTOMERLIST,
       activeCustomer: CUSTOMERLIST[0],
+      nameInput: "",
+      companyInput: "",
+      emailInput: "",
+      phoneInput: "",
     };
 
     this.setActiveCustomer = this.setActiveCustomer.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
   }
 
   setActiveCustomer(customer) {
@@ -21,17 +27,45 @@ class CustomerSection extends Component {
     console.log(`Customer changed: ${customer.state.name}`);
   }
 
+  toggleModal() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen,
+    });
+  }
+
   render() {
     const customers = this.state.customerList;
 
     return (
       <section id="customer-section">
         <ListGroup>
+          <Button onClick={this.toggleModal}>New Customer +</Button>
           {customers.map((customer) => (
             <RenderCustomerLi name={customer.state.name} key={customer.state.id} customerObj={customer} setActiveCustomer={this.setActiveCustomer} />
           ))}
         </ListGroup>
         <RenderCustomerProfile customer={this.state.activeCustomer} />
+
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}>New Customer</ModalHeader>
+          <ModalBody>
+            <Form onSubmit={this.handleLogin}>
+              <FormGroup>
+                <Label htmlFor="name">Name</Label>
+                <Input type="text" id="name" name="name" />
+              </FormGroup>
+              <FormGroup>
+                <Label htmlFor="phone">Phone</Label>
+                <Input type="text" id="phone" name="phone" />
+              </FormGroup>
+              <FormGroup>
+                <Label htmlFor="email">Email</Label>
+                <Input type="text" id="email" name="email" />
+              </FormGroup>
+              <Button type="submit" value="submit" color="primary">Add Customer</Button>
+            </Form>
+          </ModalBody>
+        </Modal>
       </section>
     );
   }
